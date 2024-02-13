e79f72b3b0d486334b0f8495ba5d2e929c552169
luis6928 <luisponcegarrido28@gmail.com>
2024-02-13 20:30:13 +0100

    update

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -3,5 +3,4 @@
 chmod +x /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/gradlew
 
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-./gradlew build
 ./gradlew war
